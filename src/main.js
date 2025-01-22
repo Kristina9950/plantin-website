e00dcefc942f028f95b0c9616579bc95a66aeeee
kristina lednicka <kristina.lednicka@student.howest.be>
2025-01-22 01:42:36 +0100

    type in profreader rectangle html, css, js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,17 @@
 import './style.css'
 
+const solutionButton = document.querySelector('.solution-button');
+const solutionDiv = document.querySelector('.solution');
+const closeSolutionButton = document.querySelector('.fa-times');
+
+function showSolution() {
+    solutionDiv.classList.remove('hidden');
+}
+function closeSolution() {
+    solutionDiv.classList.add('hidden');
+}
+
+// Add the event listener
+solutionButton.addEventListener('click', showSolution);
+closeSolutionButton.addEventListener('click', closeSolution);
 
